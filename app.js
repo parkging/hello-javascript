4def56b7bc9187c454be311ee18ea3496db01b4a
parkging <parkjun611@gmail.com>
2023-03-21 20:46:14 +0900

    using localStorage

--- app.js
+++ app.js
@@ -1,49 +1,27 @@
-const click = document.querySelector("#click");
-console.log(click);
+const loginForm = document.querySelector("#login-form");
+const helloMessage = document.querySelector("#hello");
+const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "userName";
+const savedUserName = localStorage.getItem(USERNAME_KEY);
 
-function handleTitleClick() {
-  //   activeClassName = "active";
-  //   if (this.classList.contains(activeClassName)) {
-  //     this.classList.remove(activeClassName);
-  //   } else {
-  //     this.classList.add(activeClassName);
-  //   }
-  this.classList.toggle("active");
+function clickLoginButton(event) {
+  event.preventDefault();
+  const userName = document.querySelector("#userName").value;
+  localStorage.setItem(USERNAME_KEY, userName);
+  helloMessage.innerText = "hello " + userName;
+  loginForm.classList.add(HIDDEN_CLASS);
+  helloMessage.classList.remove(HIDDEN_CLASS);
 }
 
-function handleMouseEnteer() {
-  this.innerText = "Mouse is here!";
+function paintHelloMessage(userName) {
+  loginForm.classList.add(HIDDEN_CLASS);
+  helloMessage.classList.remove(HIDDEN_CLASS);
+  helloMessage.innerText = "hello " + userName;
 }
 
-function handleMouseLeave() {
-  this.innerText = "Mouse is gone!";
+if (!savedUserName) {
+  loginForm.classList.remove(HIDDEN_CLASS);
+  loginForm.addEventListener("submit", clickLoginButton);
+} else {
+  paintHelloMessage(savedUserName);
 }
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no wifi!!");
-}
-
-function handleWindowOnline() {
-  alert("All Good!");
-}
-
-// click.onclick = handleTitleClick;
-// click.onmouseenter = handleMouseEnteer;
-// click.onmouseleave = handleMouseLeave;
-
-click.addEventListener("click", handleTitleClick);
-click.addEventListener("mouseenter", handleMouseEnteer);
-click.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
